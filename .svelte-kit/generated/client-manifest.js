5bd1efd01ce31effdbcdd2f261b26757f39dcabf
dredtrake <dretrake@gmail.com>
2023-05-21 00:16:49 +0200

    working case, need design

--- .svelte-kit/generated/client-manifest.js
+++ .svelte-kit/generated/client-manifest.js
@@ -1,19 +1,15 @@
-export { matchers } from "./client-matchers.js";
+export { matchers } from './client-matchers.js';
 
-export const nodes = [
-  () => import("./nodes/0"),
-  () => import("./nodes/1"),
-  () => import("./nodes/2"),
-];
+			export const nodes = [() => import('./nodes/0'),
+	() => import('./nodes/1'),
+	() => import('./nodes/2')];
 
-export const server_loads = [];
+			export const server_loads = [];
 
-export const dictionary = {
-  "/": [2],
+			export const dictionary = {
+	"/": [2]
 };
 
-export const hooks = {
-  handleError: ({ error }) => {
-    console.error(error);
-  },
-};
+			export const hooks = {
+				handleError: (({ error }) => { console.error(error) }),
+			};
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,53 +3,64 @@
   // import { PieChart } from '../utils/PieCharts';
   import { scaleOrdinal } from 'd3-scale';
   import { arc } from 'd3-shape';
-  import ColorScheme from 'color-scheme';
+  import randomColor from 'randomcolor';
 
-  const scheme = new ColorScheme;
-  scheme.from_hue(32)         // Start the scheme 
-      .scheme('triade')     // Use the 'triade' scheme, that is, colors
-                            // selected from 3 points equidistant around
-                            // the color wheel.
-      // .variation('soft');   // Use the 'soft' color variation
- 
-  const c: [] = scheme.colors();
-  var lots = ["LoT 1","Lot 2","lolos","Lot 4","Lot 5"];
-  const data = [40, 40, 40, 40, 40];
-  const colors = scaleOrdinal()
-    .domain(data.map((d, i) => i))
-    .range([`#${c[0]}`, `#${c[1]}`, `#${c[9]}`, `#${c[6]}`, `#${c[8]}`]);
+  const lots = ['Lot 1','Lot 2','lolos','Lot 4','Lot 5'];
+  const res = Math.round(Math.random() * lots.length - 1);
+  
+  console.log(`res should be ${res}`);
 
-  let svgWidth = 200;
-  let svgHeight = 200;
-  let radius = Math.min(svgWidth, svgHeight) / 2;
+  let css = '';
 
-  let pie = arc()
-    .innerRadius(0)
-    .outerRadius(radius);
+  const svgWidth = 200;
+  const svgHeight = 200;
 
-  let total = data.reduce((acc, val) => acc + val, 0);
-
-  let angles = data.reduce((acc, val, i) => {
-    let start = acc[i - 1] || 0;
-    let angle = start + (val / total) * 2 * Math.PI;
-    return [...acc, angle];
-  }, []);
-  console.log({ data })
-  let rotation = 0;
- let css = '';
-  function handleClick(event: MouseEvent): void {
+  const handleClick = (event: MouseEvent): void => {
     event.preventDefault();
-    rotation = Math.round(Math.random() * 150);
-    css = 'transform: rotate(1480deg); transition: transform  5s ease-in-out';
+    // rotation = Math.round(Math.random() * 360) + ((3 + Math.round(Math.random() * 5)) * 360);
+    const l = lots.length;
+    const rad = 1 / l;
+    let rotation = 0;
+
+    rotation = Math.round(((1 - rad) - (rad * res)) * 360) + ((3 + Math.round(Math.random() * 5)) * 360);
+    css = `transform: rotate(${rotation}deg); transition: transform  5s ease-in-out`;
+  }
+  const createTwistingPie = (items: string[]) => {
+    const lotFormated = lots.map(item => ({ item , data: 1}));
+
+    const rColors = randomColor({
+        count: lots.length,
+        luminosity: 'light',
+        hue: 'random',
+      });
+
+    const data = [40, 40, 40, 40, 40];
+    const colors = scaleOrdinal()
+      .range(rColors);
+
+    
+    const radius = Math.min(svgWidth, svgHeight) / 2;
+
+    const pie = arc()
+      .innerRadius(0)
+      .outerRadius(radius);
+
+    const total = lotFormated.reduce((acc, { data }) => acc + data, 0);
+
+    let angles = lotFormated.reduce((acc, { data }, i) => {
+      let start = acc[i - 1] || 0;
+      let angle = start + (data / total) * 2 * Math.PI;
+      return [...acc, angle];
+    }, []);
+    return { 
+      angles,
+      colors,
+      pie,
+    };
   }
 
-  const population = [
-    {name: "truc", value: 10 },
-    {name: "machin", value: 20 },
-    {name: "trucbof", value: 20 },
-    {name: "truczadzd", value: 20 },
-    {name: "truczadzd5", value: 20 },
-  ]
+  const { angles, colors, pie } = createTwistingPie(lots);
+
 </script>
 <style>
   .thick {
@@ -73,16 +84,16 @@
     on:click={handleClick}
     on:keydown={handleClick}
   >
-  {#each data as value, i}
+  {#each angles as value, i}
     <path
       d={pie({
         startAngle: angles[i - 1] || 0,
-        endAngle: angles[i],
+        endAngle: value,
       })}
           fill={colors(i)}
     />
     {/each}
-    {#each data as value, i}
+    {#each angles as value, i}
     <text transform={`rotate(${angles[i] * (180 / Math.PI)})`}  y="15%" writing-mode="tb" font-size="6px">{lots[i]}</text>
     {/each}
   </svg>
